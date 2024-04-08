3153410657788bfc94cf4868d93456960a1f278e
Seth Nowlin <seth@lafayette-parish.com>
2024-04-07 23:18:01 -0500

    adding variables for builds/non-knowledgeable

---
+++ riscv-toolchain/lib/gcc/riscv64-linux/13.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/arnd/git/gcc/configure --host=x86_64-linux-gnu --build=aarch64-linux --target=riscv64-linux --enable-targets=all --prefix=/home/arnd/cross/x86_64/gcc-13.2.0-nolibc/riscv64-linux --enable-languages=c --without-headers --disable-bootstrap --disable-nls --disable-threads --disable-shared --disable-libmudflap --disable-libssp --disable-libgomp --disable-decimal-float --disable-libquadmath --disable-libatomic --disable-libcc1 --disable-libmpx --enable-checking=release --with-static-standard-libraries";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "lp64d" }, { "arch", "rv64imafdc_zicsr_zifencei" }, { "isa_spec", "20191213" } };
